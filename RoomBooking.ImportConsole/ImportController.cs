a437d4a002f0184d7bc57cc8ed7becba8e273ef8
j07122000 <janine.hoellhuber@gmx.at>
2020-11-03 22:48:55 +0100

    Add project files.

---
+++ RoomBooking.ImportConsole/ImportController.cs
@@ -0,0 +1,48 @@
+﻿using RoomBooking.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Utils;
+
+namespace RoomBooking.ImportConsole
+{
+  public static class ImportController
+  {
+        /// <summary>
+        /// Liest die Buchungen mit ihren Räumen und Kunden aus der
+        /// csv-Datei ein.
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<IEnumerable<Booking>> ReadBookingsFromCsvAsync()
+        {
+            string[][] matrix = await MyFile.ReadStringMatrixFromCsvAsync("bookings.csv", true);
+            var customers = matrix
+              
+                .Select(cust => new Customer
+                {
+                    FirstName = cust[1],
+                    LastName = cust[0],
+                    Iban = cust[2]
+
+                }).GroupBy(line => line.FirstName + line.LastName + line.Iban).Select(s=> s.First()).ToArray();
+
+            var rooms = matrix
+                .GroupBy(line => line[3])
+                .Select(ro => new Room
+                {
+                    RoomNumber = ro.Key
+                }).ToArray();
+
+            var bookings = matrix
+                .Select(book => new Booking
+                {
+                    Customer = customers.Single(s => book[0] == s.LastName && book[1] == s.FirstName && book[2] == s.Iban),
+                    Room = rooms.Single(s => book[3] == s.RoomNumber),
+                    From = book[4],
+                    To = book[5]
+                }).ToArray();
+            return bookings;
+    }
+  }
+}
---
+++ RoomBooking.Wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,118 @@
+﻿using RoomBooking.Core.Contracts;
+using RoomBooking.Core.Entities;
+using RoomBooking.Persistence;
+using RoomBooking.Wpf.Common;
+using RoomBooking.Wpf.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace RoomBooking.Wpf.ViewModels
+{
+  public class MainViewModel : BaseViewModel
+  {
+        public ObservableCollection<Booking> _booking;
+        /*public ObservableCollection<Room> _room;
+        public ObservableCollection<Customer> _customer;*/
+        public string _to;
+        public string _from;
+
+        public ObservableCollection<Booking> Booking
+        {
+            get => _booking;
+            set
+            {
+                _booking = value;
+                OnPropertyChanged(nameof(Booking));
+            }
+        }
+
+        /*public ObservableCollection<Room> Room
+        {
+            get => _room;
+            set
+            {
+                _room = value;
+                OnPropertyChanged(nameof(Room));
+            }
+        }
+
+        public ObservableCollection<Customer> Customer
+        {
+            get => _customer;
+            set
+            {
+                _customer = value;
+                OnPropertyChanged(nameof(Customer));
+            }
+        }
+        */
+        public string From
+        {
+            get => _from;
+            set
+            {
+                _from = value;
+                OnPropertyChanged(nameof(From));
+            }
+        }
+
+        public string To
+        {
+            get => _to;
+            set
+            {
+                _to = value;
+                OnPropertyChanged(nameof(To));
+            }
+        }
+        public MainViewModel(IWindowController windowController) : base(windowController)
+        {
+            LoadDataAsync();
+        }
+
+        private  Task LoadDataAsync()
+        {
+            using IUnitOfWork uow = new UnitOfWork();
+            var bookings =  uow.Bookings
+               
+              .GetAllAsync();
+
+
+
+           // Booking = new ObservableCollection<Booking>(bookings);
+            return bookings;
+
+        }
+
+        public static async Task<MainViewModel> CreateAsync(IWindowController windowController)
+        {
+            var viewModel = new MainViewModel(windowController);
+            await viewModel.LoadDataAsync();
+            return viewModel;
+        }
+
+        public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+             throw new NotImplementedException();
+        }
+
+        private string _filterText = "";
+        public string FilterText
+        {
+            get
+            {
+                return _filterText;
+            }
+
+            set
+            {
+                _filterText = value;
+                LoadEmployees();
+
+            }
+        }
+    }
+}
